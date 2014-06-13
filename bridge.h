f3cd73995bfb95910e33f77d6e491dd41d704df8
root <root@localhost.localdomain>
2014-06-13 16:47:47 +0800

    discard default param

--- bridge.h
+++ bridge.h
@@ -16,8 +16,8 @@ namespace levelque {
 		Bridge(){}
 		~Bridge(){}
 		virtual bool CreateQueueName(const string &path, const string &name);
-		virtual bool Read(const std::string &queueName, std::string &str, uint8_t level=0);
-		virtual bool Write(const std::string &queueName, char *str, uint64_t length, uint8_t level=0);
+		virtual bool Read(const std::string &queueName, std::string &str, uint8_t level);
+		virtual bool Write(const std::string &queueName, char *str, uint64_t length, uint8_t level);
 		virtual uint32_t Size(const std::string &queueName, uint8_t level);
 		virtual uint32_t Size(const std::string &queueName);		
  };
--- include/queue.h
+++ include/queue.h
@@ -17,8 +17,8 @@ namespace levelque {
 		
 		}
 		virtual bool CreateQueueName(const std::string &path, const std::string &name) = 0;
-		virtual bool Read(const std::string &queueName, std::string &str, uint8_t level=0) = 0;
-		virtual bool Write(const std::string &queueName, char *str, uint64_t length, uint8_t level=0)=0;
+		virtual bool Read(const std::string &queueName, std::string &str, uint8_t level) = 0;
+		virtual bool Write(const std::string &queueName, char *str, uint64_t length, uint8_t level)=0;
 		virtual uint32_t Size(const std::string &queueName, uint8_t level) = 0;
 		virtual uint32_t Size(const std::string &queueName) = 0;
 	};
